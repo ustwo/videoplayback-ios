d4bdb7fb67ea4373da2a7caeca178637f715a6c8
Sonam <sdhingra89@gmail.com>
2017-04-21 10:53:48 -0400

    set up basic projectand targets

---
+++ VideoPlaybackKit/VideoPlaybackKitiOS/VideoPlaybackKitiOS.h
@@ -0,0 +1,19 @@
+//
+//  VideoPlaybackKitiOS.h
+//  VideoPlaybackKitiOS
+//
+//  Created by Sonam on 4/20/17.
+//  Copyright © 2017 ustwo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for VideoPlaybackKitiOS.
+FOUNDATION_EXPORT double VideoPlaybackKitiOSVersionNumber;
+
+//! Project version string for VideoPlaybackKitiOS.
+FOUNDATION_EXPORT const unsigned char VideoPlaybackKitiOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VideoPlaybackKitiOS/PublicHeader.h>
+
+
